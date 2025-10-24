19c8a1e0f654110bd5ca90529ae5f8f8b53125b8
Skobelkin98 <pnevajno@gmail.com>
2025-10-24 23:03:56 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,27 +1,15 @@
 import asyncio
-from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message, WebAppInfo, InlineKeyboardMarkup, InlineKeyboardButton
+from aiogram import Bot
 
 TOKEN = "8219879166:AAHpbP7T35gTV1Ry1F9T37c69mzbt_RehDw"
-
 bot = Bot(token=TOKEN)
-dp = Dispatcher()
 
-# Ссылка на наше веб-приложение
 WEB_APP_URL = "https://talant-webapp-production.up.railway.app"
 
-@dp.message(F.text == "/start")
-async def start(message: Message):
-    kb = InlineKeyboardMarkup(inline_keyboard=[
-        [InlineKeyboardButton(text="🚀 Открыть систему талантов", web_app=WebAppInfo(url=WEB_APP_URL))]
-    ])
-    await message.answer("Привет! 👋\nНажми, чтобы открыть систему талантов:", reply_markup=kb)
-
-
 async def main():
-    print("Бот запущен...")
+    print("Установка webhook...")
     await bot.set_webhook(url="https://talant-webapp-production.up.railway.app/webhook")
-    await dp.start_polling(bot)
+    print("Webhook успешно установлен. Бот завершил работу.")
 
 if __name__ == "__main__":
     asyncio.run(main())
